5f47b7094b7d9d65bdb48b8457566a4b3fffa708
kwonssshyeon <kwonsh4396@gmail.com>
2023-11-02 22:31:40 +0900

    fix: method 변경

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.responses import JSONResponse
+
 app = FastAPI()
 
-@app.get("/")
+class TestResponseBody(BaseModel):
+    test_message:str
+
+
+
+@app.post("/")
 def 이름():
-  return '표주시게'
+    return '표주시게'
+
+@app.post("/test", response_class=TestResponseBody)
+def 이름():
+    result = {
+        "test_message":"안녕하세용가리"
+    }
+    return JSONResponse(result)
+
+@app.post("/test/kakao")
+def 이름():
+    response = {
+        "version":"2.0",
+        "template":{
+            "outputs":[
+                {
+                    "test_msg":{
+                        "test":"안녕하세용가리치킨"
+                    }
+                }
+            ]
+        }
+    }
+    return JSONResponse(response)
