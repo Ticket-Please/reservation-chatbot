e7fb66801cab8e8c123ce094fd9eef54391fe8a5
rnjs5540 <rnjs5540@gmail.com>
2023-11-10 22:13:34 +0900

    feat: 이전 대화 기억 구현
    - 유저 발화 db에 넣기
    - db에서 해당유저 관련 대화들 불러와 gpt매개변수에 넣기
    - gpt답변도 db에 넣기

--- chatGPT/test.py
+++ chatGPT/test.py
@@ -2,46 +2,69 @@ import json
 import openai
 import httpx
 import asyncio
+from pymongo import MongoClient
 
-with open('./secret.json') as f:
+with open('./secret.json', encoding='utf-8') as f:
     secrets = json.loads(f.read())
 API_KEY = secrets["ChatGPT-key"]
+MONGO_URI = secrets["MONGO_URI"]
+
 openai.api_key = API_KEY
+mongoose = MongoClient(MONGO_URI)
+db = mongoose['TicketPlz']
+collection = db['userMessages']
 
-response = {
-    "version": "2.0",
-    "template": {
-        "outputs": [
-            {
-                "simpleText": {
-                    "text": "안녕하세용가리치킨"
-                }
-            }
-        ]
-    }
-}
-
-response_callback = {
-    "version": "2.0",
-    "useCallback": True,
-}
+with open('./schema.json', encoding='utf-8') as f:
+    schema = json.loads(f.read())
+response = schema['response']
+response_callback = schema['response_callback']
 
 
+# 테스트 함수들
 def test_call(request_data):
     user_message = "기차 예매를 하는 방법을 쉽게 설명해줘"
     callback_url = request_data['userRequest']['callbackUrl']
-    asyncio.create_task(send_callback(callback_url, user_message))  # 콜백 요청을 비동기적으로 보냄
+    asyncio.create_task(get_real_answer(callback_url, user_message))
     return response_callback
 
-def get_answer(request_data):
+def test_db_save(request_data):
     user_message = request_data['userRequest']['utterance']
-    callback_url = request_data['userRequest']['callbackUrl']
-    asyncio.create_task(send_callback(callback_url, user_message))  # 콜백 요청을 비동기적으로 보냄
+    user_id = request_data['userRequest']['user']['id']
+    print(user_message)
+    collection.insert_one({'user_id':user_id, 'role':'user', 'content':user_message})
+    return {"굿굿"}
+
+def test_db_load(user_id: str):
+    messages = []
+    conversations = collection.find({"user_id":user_id})
+    for converstaion in conversations:
+        del converstaion['_id']
+        print(converstaion)
+        messages.append(converstaion)
+    print(messages)
+    return messages
+
+# 여기부터 진짜!
+def get_early_answer(request_data):
+    asyncio.create_task(get_real_answer(request_data))
     return response_callback
 
-    
-async def send_callback(callback_url: str, user_message: str):
-    messages = [{'role':'user', 'content':user_message}]
+async def get_real_answer(request_data):
+    # 유저 대화 DB저장
+    user_message = request_data['userRequest']['utterance']
+    user_id = request_data['userRequest']['user']['id']
+    callback_url = request_data['userRequest']['callbackUrl']
+    collection.insert_one({'user_id':user_id, 'role':'user', 'content':user_message})
+
+    # DB에서 대화들 불러와 매개변수에 넣기
+    messages = []
+    conversations = collection.find({"user_id":user_id})
+    for converstaion in conversations:
+        del converstaion['_id']
+        print(converstaion)
+        messages.append(converstaion)
+
+    # gpt답변 생성
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-0613",
         messages=messages,
@@ -50,9 +73,12 @@ async def send_callback(callback_url: str, user_message: str):
         stop=None,
         temperature=0.5,
     )
+
+    # gpt답변 DB에 저장
     gpt_answer = completion['choices'][0]['message']['content']
-    response["template"]["outputs"][0]["simpleText"]["text"] = gpt_answer
-    
+    collection.insert_one({'user_id':user_id, 'role':'system', 'content':gpt_answer})
+
     # HTTP 클라이언트를 사용하여 비동기적으로 POST 요청을 보냄
+    response["template"]["outputs"][0]["simpleText"]["text"] = gpt_answer
     async with httpx.AsyncClient() as client:
         await client.post(callback_url, json=response)
--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from starlette.responses import JSONResponse
-from chatGPT.test import test_call, get_answer
+from chatGPT.test import test_call, get_early_answer, test_db_save, test_db_load
+import json
 
 app = FastAPI()
 
 class TestResponseBody(BaseModel):
     test_message:str
 
-
+with open('./schema.json', encoding='utf-8') as f:
+    schema = json.loads(f.read())
+dummyUserAPI = schema['dummyUserAPI']
 
 @app.post("/")
 def 이름():
-    return '표주시게'
+    return 'post표주시게'
+@app.get("/")
+def 이름():
+    return 'get표주시게'
 
 @app.post("/test", response_class=TestResponseBody)
 def 이름():
@@ -42,9 +48,17 @@ async def func(request: Request):
     json_data = await request.json()
     return test_call(json_data)
 
+@app.post('/saveTest')
+async def func():
+    return test_db_save(dummyUserAPI)
 
+@app.post('/loadTest')
+def func():
+    user_id = dummyUserAPI['userRequest']['user']['id']
+    return test_db_load(user_id)
+
+# 이게 진짜!
 @app.post('/question')
 async def func(request: Request):
     json_data = await request.json()
-    return get_answer(json_data)
-
+    return get_early_answer(json_data)
